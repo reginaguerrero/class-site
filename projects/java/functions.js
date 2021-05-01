0b50b5de7dcdd24ef91aa0dd4f1251a20c89710e
reginaguerrero <78653931+reginaguerrero@users.noreply.github.com>
2021-05-01 11:09:18 -0400

    avance

--- projects/java/functions.js
+++ projects/java/functions.js
@@ -5,7 +5,7 @@
 */
 $('.marquee').marquee({
 	//duration in milliseconds of the marquee
-	duration: 15000,
+	duration: 16000,
 	//gap in pixels between the tickers
 	gap: 50,
 	//time in milliseconds before the marquee will start animating
